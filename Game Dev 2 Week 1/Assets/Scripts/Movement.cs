f1c0af20a450cba6e3a175e9d8ea70ae8f7b1ee3
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-08-27 10:27:26 -0400

    Working on Flamethrower

--- Game Dev 2 Week 1/Assets/Scripts/Movement.cs
+++ Game Dev 2 Week 1/Assets/Scripts/Movement.cs
@@ -8,7 +8,7 @@ public class Movement : MonoBehaviour
     public float sensitivity = 10f;
     private Vector2 currentRotation;
     public GameObject player;
-
+    public GameObject flameThrower;
     private void Update()
     {
         if (Input.GetKey(KeyCode.W))
@@ -32,5 +32,15 @@ public class Movement : MonoBehaviour
         gameObject.transform.rotation = Quaternion.Euler(0, currentRotation.x, 0);
         if (Input.GetMouseButtonDown(0))
             Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("button is down");
+            flameThrower.GetComponent<ParticleSystem>().Play();
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            print("up");
+            // flameThrower.GetComponent<ParticleSystem>().Pause();
+        }
     }
 }
