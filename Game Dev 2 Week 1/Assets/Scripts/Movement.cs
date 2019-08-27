a69968cc90d4525c6741b45949775245a7f8a1f4
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-08-27 14:42:43 -0400

    Flamethrower
    Got the flamethrower working

--- Game Dev 2 Week 1/Assets/Scripts/Movement.cs
+++ Game Dev 2 Week 1/Assets/Scripts/Movement.cs
@@ -8,8 +8,28 @@ public class Movement : MonoBehaviour
     public float sensitivity = 10f;
     private Vector2 currentRotation;
     public GameObject player;
-    public GameObject flameThrower;
+    public ParticleSystem flameThrower;
+    private void Start()
+    {
+        flameThrower.Stop();
+    }
     private void Update()
+    {
+        Move();
+        RotateObject();
+        if (Input.GetMouseButtonDown(0))
+            Cursor.lockState = CursorLockMode.Locked;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            flameThrower.Play();
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            flameThrower.Stop();
+        }
+        print(flameThrower.isPlaying);
+    }
+    private void Move()
     {
         if (Input.GetKey(KeyCode.W))
         {
@@ -27,20 +47,11 @@ public class Movement : MonoBehaviour
         {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
         }
+    }
+    private void RotateObject()
+    {
         currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
-        currentRotation.x = Mathf.Repeat(currentRotation.x, 360); 
+        currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
         gameObject.transform.rotation = Quaternion.Euler(0, currentRotation.x, 0);
-        if (Input.GetMouseButtonDown(0))
-            Cursor.lockState = CursorLockMode.Locked;
-        if (Input.GetKey(KeyCode.Space))
-        {
-            print("button is down");
-            flameThrower.GetComponent<ParticleSystem>().Play();
-        }
-        if(Input.GetKeyUp(KeyCode.Space))
-        {
-            print("up");
-            // flameThrower.GetComponent<ParticleSystem>().Pause();
-        }
     }
 }
