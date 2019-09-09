e4891047da384dc7fad5ed2a6411016e9977a570
aj418014 <47157670+aj418014@users.noreply.github.com>
2019-09-09 13:37:35 -0400

    Added Burnie
    Burnie is now more upset the more you burn

---
+++ Game Dev 2 Week 1/Assets/burnieManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class burnieManager : MonoBehaviour
+{
+    public GameObject player;
+    float money;
+    public GameObject[] bernies;
+    // Start is called before the first frame update
+    void Start()
+    { 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        money = player.GetComponent<Money>().funds;  
+        if(money < 200)
+        {
+            if(bernies[0].activeInHierarchy == false)
+               bernies[0].SetActive(true);
+        }
+        if(money > 200 && money < 1000)
+        {
+            if (bernies[1].activeInHierarchy == false)
+            {
+                bernies[0].SetActive(false);
+                bernies[1].SetActive(true);
+            }
+        }
+        if(money > 1000 && money < 10000)
+        {
+            if (bernies[2].activeInHierarchy == false)
+            {
+                bernies[1].SetActive(false);
+                bernies[2].SetActive(true);
+            }
+        }
+        if(money > 10000)
+        {
+            if(bernies[3].activeInHierarchy == false)
+            {
+                bernies[3].SetActive(true);
+                bernies[2].SetActive(false);
+            }
+        }
+    }
+}
